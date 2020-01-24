439cb7ec0bbcf75b5304c8119430bbeab88eefcc
magikitty <elina.satola@gmail.com>
2020-01-24 14:11:05 +0200

    Create GetFirstPlayer function to pick who starts the game

--- paskahousut.py
+++ paskahousut.py
@@ -1,5 +1,8 @@
-welcome_message = "This is a simple game of Paskahousut. Get ready to play!"
+# Import package to generate random numbers
+import random
 
+# Welcome message for player
+welcome_message = "This is a simple game of Paskahousut. Get ready to play!"
 print(welcome_message)
 
 
@@ -11,25 +14,45 @@ def GetName():
 
 
 #GameOver determines whether or not the game is over for testing purposes
-# def GameOver():
+# def GameIsOver():
 #     counter = 0
 #     for num in range(1-11):
 #         if counter < 10:
 #             GameOver = False
 #             counter += 1
-#             return GameOver
 #         elif counter >= 10:
-#             GameOver = True
-#             return GameOver
+#             break
+#     return GameOver
+
+
+# Set first player (as Player or Computer) to start game
+def SetFirstPlayer():
+    # print("Here is a random number:")
+    # print(random.randint(0, 1))
+    start_num = random.randint(0, 1)
+    # print(start_num)
+    if start_num == 0:
+        PlayerFirst = True
+        print("The Player goes first.")
+    elif start_num == 1:
+        PlayerFirst = False
+        print("The Computer goes first.")
+    return PlayerFirst
+
 
 # GameLoop alternates between the player's and computer's turn
 def GameLoop():
     print("The game loop has started.")
     GameOver = False
+    PlayerFirst = SetFirstPlayer()
     while GameOver == False:
-        print("The game is not over!")
-        PlayerTurn()
-        ComputerTurn()
+        print("The game is not over!") 
+        if PlayerFirst == True:
+            PlayerTurn()
+            ComputerTurn()
+        elif PlayerFirst == False:
+            ComputerTurn()
+            PlayerTurn()
         break
     GameOver = True
     print("The game is over!")
