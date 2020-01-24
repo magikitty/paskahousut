a3523e334e659c30db3f707083f4caa5a4356871
magikitty <elina.satola@gmail.com>
2020-01-24 11:12:48 +0200

    Create simple game loop that alternates between player turns

--- paskahousut.py
+++ paskahousut.py
@@ -10,4 +10,47 @@ def GetName():
     return player_name
 
 
-GetName()
+#GameOver determines whether or not the game is over for testing purposes
+# def GameOver():
+#     counter = 0
+#     for num in range(1-11):
+#         if counter < 10:
+#             GameOver = False
+#             counter += 1
+#             return GameOver
+#         elif counter >= 10:
+#             GameOver = True
+#             return GameOver
+
+# GameLoop alternates between the player's and computer's turn
+def GameLoop():
+    print("The game loop has started.")
+    GameOver = False
+    while GameOver == False:
+        print("The game is not over!")
+        PlayerTurn()
+        ComputerTurn()
+        break
+    GameOver = True
+    print("The game is over!")
+    print("The game loop has ended.")
+
+
+# PlayerTurn starts player's turn
+def PlayerTurn():
+    print("It's the player's turn!")
+
+
+# ComputerTurn starts computer's turn
+def ComputerTurn():
+    print("It's the computer's turn!")
+
+
+# Game() calls the functions that make the game run
+def Game():
+    GetName()
+    GameLoop()
+
+
+# Call Game() to start game
+Game()
