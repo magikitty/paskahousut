9d85def517c4643677204281f0232f92b71e32a7
magikitty <elina.satola@gmail.com>
2020-01-25 18:59:19 +0200

    Create readFromFile()

--- data_read_write.py
+++ data_read_write.py
@@ -3,3 +3,8 @@
 def writeToFile(content, fileToWriteTo):
     doc = open(fileToWriteTo, "w")
     doc.write(content)
+
+
+def readFromFile(fileToReadFrom):
+    doc = open(fileToReadFrom, "r")
+    return doc.read()
