ca5a45ed864bda45dccd12a9a9f333a04c418eae
magikitty <elina.satola@gmail.com>
2020-01-26 19:29:52 +0200

    Create dealCardsToPlayer() and dealCardsToComputer()

--- data_cards.py
+++ data_cards.py
@@ -5,6 +5,8 @@ import random
 DATA_ALL_CARDS = "data_immutable/all_cards.txt"
 DATA_DECK = "data_mutable/cards_in_deck.txt"
 COUNTER_CARDS = "of"
+HAND_PLAYER = "data_mutable/player_hand.txt"
+HAND_COMPUTER = "data_mutable/computer_hand.txt"
 
 
 # Copy cards from all_cards.txt to cards_in_deck.txt to populate the deck
@@ -25,7 +27,7 @@ def getCardNumber(cardNumber):
 
 def getRandomCard():
     max_number = countCardsInDeck()
-    random_number = random.randint(0, max_number)
+    random_number = random.randint(0, max_number - 1)
     random_card = getCardNumber(random_number)
     return random_card
 
@@ -33,3 +35,15 @@ def getRandomCard():
 def countCardsInDeck():
     amountCards = data_read_write.countContent(COUNTER_CARDS, DATA_DECK)
     return amountCards
+
+
+def dealCardsToPlayer():
+    card = getRandomCard()
+    data_read_write.writeToFile(card, HAND_PLAYER)
+    # add functionality to remove card after it is dealt from deck
+
+
+def dealCardsToComputer():
+    card = getRandomCard()
+    data_read_write.writeToFile(card, HAND_COMPUTER)
+    # add functionality to remove card after it is dealt from deck
