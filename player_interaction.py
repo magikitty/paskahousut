af52c8cafcfea847eba06a2828d155679066278d
magikitty <elina.satola@gmail.com>
2020-01-30 19:23:55 +0200

    Split getInput() into two functions, create processPlayerTUrn()

--- player_interaction.py
+++ player_interaction.py
@@ -3,9 +3,12 @@ import readchar
 
 HAND_PLAYER = "data_mutable/player_hand.txt"
 HAND_COMPUTER = "data_mutable/computer_hand.txt"
-INSTRUCTIONS_INTERACTION = ("Press P to play a card from your hand.\n" +
+INSTRUCTIONS_INTERACTION = (
+    "Press S to show cards in your hand.\n"
+    "Press P to play a card from your hand.\n" +
     "Press D to draw a card and immediately play it.\n" +
-    "Press I to pick up all cards in the pile.")
+    "Press I to pick up all cards in the pile."
+    )
 
 
 def displayHand(docHandToDisplay):
@@ -18,17 +21,25 @@ def displayPlayerHand():
 
 
 def getInput():
-    while True:
-        print(INSTRUCTIONS_INTERACTION)
-        char = readchar.readchar().lower()
-        if char == "p":
-            print("You are playing a card from your hand")
-        elif char == "d":
-            print("You are drawing a card and playing it")
-        elif char == "i":
-            print("You are picking up all the cards in the pile")
-        else:
-            print("Please press one of the specified keys")   # why is this called??
-        break
-    print("Pressed key: " + char)
-    return(char)
+    print(INSTRUCTIONS_INTERACTION)
+    char = ""
+
+    while char != "s" or "p" or "d" or "i":
+        char = readchar.readchar().lower() 
+        if char == "s" or "p" or "d" or "i":
+            return char
+
+
+def playerAction(char):
+    if char == "s":
+        displayPlayerHand()
+    elif char == "p":
+        print("You are playing a card from your hand")
+    elif char == "d":
+        print("You are drawing a card and playing it")
+    elif char == "i":
+        print("You are picking up all the cards in the pile")
+
+
+def processPlayerTurn():
+    playerAction(getInput())
