eb95b0061fe9d7d2013426071ba3bfb8fb0f23c0
magikitty <elina.satola@gmail.com>
2020-02-02 15:38:40 +0200

    Create constants.py and move all constants into it

---
+++ constants.py
@@ -0,0 +1,28 @@
+# Data files
+DATA_ALL_CARDS = "data_immutable/all_cards.txt"
+DATA_DECK = "data_mutable/cards_in_deck.txt"
+DATA_PLAYER_NAME = "data_mutable/player_name.txt"
+COUNTER_CARDS = "of"
+HAND_PLAYER = "data_mutable/player_hand.txt"
+HAND_COMPUTER = "data_mutable/computer_hand.txt"
+PILE_CARDS = "data_mutable/cards_in_pile.txt"
+
+# Messages
+MESSAGE_ENTER_NAME = "Enter your name: "
+MESSAGE_WELCOME = "This is a simple game of Paskahousut. Get ready to play!"
+
+MESSAGE_PLAYER_TURN = "It's the player's turn!"
+MESSAGE_COMPUTER_TURN = "It's the computer's turn!"
+
+MESSAGE_INSTRUCTIONS_INTERACTION = (
+    "Press S to show cards in your hand.\n"
+    "Press P to play a card from your hand.\n" +
+    "Press D to draw a card and immediately play it.\n" +
+    "Press I to pick up all cards in the pile."
+    )
+
+MESSAGE_DRAW_CARD = "The card you have drawn is: "
+MESSAGE_DRAW_PLAY = "You are drawing a card and playing it"
+MESSAGE_PICK_PILE = "You are picking up all the cards in the pile"
+MESSAGE_PLAY_CARD = "You are playing a card from your hand"
+MESSAGE_SHOW_HAND = "\nHere are your cards:\n"
