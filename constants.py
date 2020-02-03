dbc49b4443216972d549e585e8293b656ee15ca3
magikitty <elina.satola@gmail.com>
2020-02-03 10:25:30 +0200

    Add MESSAGE_ALL_CARDS_IN_PILE and MESSAGE_TOP_CARD_IN_PILE

--- constants.py
+++ constants.py
@@ -27,3 +27,6 @@ MESSAGE_DRAW_PLAY = "You are drawing a card and playing it"
 MESSAGE_PICK_PILE = "You are picking up all the cards in the pile"
 MESSAGE_PLAY_CARD = "You are playing a card from your hand"
 MESSAGE_SHOW_HAND = "\nHere are your cards:\n"
+
+MESSAGE_ALL_CARDS_IN_PILE = "Here are all the cards in the pile. Card number 1 (at the top of the pile) is the most recent card played."
+MESSAGE_TOP_CARD_IN_PILE = "Here is the card at the top of the pile:"
