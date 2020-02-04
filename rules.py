c7b202a2b7c1f55bc3851c58abc6de088735bddc
magikitty <elina.satola@gmail.com>
2020-02-04 10:17:52 +0200

    Create convertValueToInt()

--- rules.py
+++ rules.py
@@ -1,6 +1,6 @@
 import player_interaction
 
-test_card_to_play = "10 of Hearts" # debugging
+test_card_to_play = "Q of Hearts" # debugging
 test_pile_card_value = 5 # debugging
 
 
@@ -14,8 +14,24 @@ def getCardValue():
         else:
             break
 
-    print(card_value)   # debugging
+    # print(card_value)   # debugging
     return card_value
+
+
+def convertValueToInt():
+    card_value = getCardValue()
+    if card_value == "J":
+        card_value_int = 11
+    elif card_value == "Q":
+        card_value_int = 12
+    elif card_value == "K":
+        card_value_int = 13
+    elif card_value == "A":
+        card_value_int = 14
+    else:
+        card_value_int = int(card_value)
+    print("the int value of", card_value, "is", card_value_int)   # debugging
+    return card_value_int
 
 
 def checkCanPlayCard():
