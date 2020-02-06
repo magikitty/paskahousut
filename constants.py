cafdb27cf038c5d3f0a52940714b63cdcdb5ab27
magikitty <elina.satola@gmail.com>
2020-02-06 17:52:08 +0200

    Modify comments

--- constants.py
+++ constants.py
@@ -7,12 +7,9 @@ HAND_PLAYER = "data_mutable/player_hand.txt"
 HAND_COMPUTER = "data_mutable/computer_hand.txt"
 PILE_CARDS = "data_mutable/cards_in_pile.txt"
 
-# Messages
+# New game messages
 MESSAGE_ENTER_NAME = "Enter your name: "
 MESSAGE_WELCOME = "This is a simple game of Paskahousut. Get ready to play!"
-
-MESSAGE_PLAYER_TURN = "It's the player's turn!"
-MESSAGE_COMPUTER_TURN = "It's the computer's turn!"
 
 MESSAGE_INSTRUCTIONS_INTERACTION = (
     "\n"
@@ -24,23 +21,28 @@ MESSAGE_INSTRUCTIONS_INTERACTION = (
     
     )
 
+# Action messages
 ACTION_ALL_CARDS_IN_PILE = "i"
 ACTION_DRAW_PLAY = "d"
 ACTION_PICK_PILE= "u"
 ACTION_PLAY_CARD = "p"
 ACTION_SHOW_HAND = "s"
 
-MESSAGE_DRAW_CARD = "The card you have drawn is: "
-MESSAGE_DRAW_PLAY = "You are drawing a card and playing it"
-MESSAGE_PICK_PILE = "You are picking up all the cards in the pile"
-MESSAGE_PLAY_CARD = "You are playing a card from your hand"
+# Action selection messages
+MESSAGE_DRAW_CARD = "\nThe card you have drawn is: "
+MESSAGE_DRAW_PLAY = "\nYou are drawing a card and playing it"
+MESSAGE_PICK_PILE = "\nYou are picking up all the cards in the pile"
+MESSAGE_PLAY_CARD = "\nYou are playing a card from your hand"
 MESSAGE_SHOW_HAND = "\nHere are your cards:\n"
 
+# Status messages
 MESSAGE_ALL_CARDS_IN_PILE = "\nHere are all the cards in the pile. Card number 1 (at the top of the pile) is the most recent card played."
 MESSAGE_CANNOT_PLAY_CARD = "\nYou cannot play that card. Choose another card or do something else."
 MESSAGE_CHOOSE_ACTION = "\nWhat do you want to do? "
 MESSAGE_CHOOSE_CARD_TO_PLAY = "\nEnter the number of the card you want to play: "
+MESSAGE_COMPUTER_TURN = "\nIt's the computer's turn!"
 MESSAGE_FOLD_PILE = "\nThe pile has been folded."
-MESSAGE_PLAYED_CARD = "\nYou play the card:"
 MESSAGE_PILE_EMPTY = "\nThere are no cards in the pile"
+MESSAGE_PLAYED_CARD = "\nYou play the card:"
+MESSAGE_PLAYER_TURN = "\nIt's the player's turn!"
 MESSAGE_TOP_CARD_IN_PILE = "\nHere is the card at the top of the pile:"
--- rules.py
+++ rules.py
@@ -20,7 +20,7 @@ def checkCanPlayCard(card_to_play, card_pile_top):
             if card_pile_top <= 13:
                 can_play_card = True
             else:
-                can_play_card = False           # IMPLEMENT card_pile_top == 3
+                can_play_card = False
 
         elif card_to_play == 7 or card_to_play == 10:
             if card_pile_top <= 13:
