e122cfd6052b1757f10f13e160565c1c8dfc9799
magikitty <elina.satola@gmail.com>
2020-02-10 15:00:03 +0200

    Create fourOfAKind()

--- rules.py
+++ rules.py
@@ -1,5 +1,7 @@
 import constants
 import data_read_write
+import player_interaction
+import value_cards
 
 
 def checkCanPlayCard(card_to_play, card_pile_top):
@@ -38,3 +40,24 @@ def checkCanPlayCard(card_to_play, card_pile_top):
 def foldDeckWithTen(card_10):
     data_read_write.clearFile(constants.PILE_CARDS)
     print(constants.MESSAGE_FOLD_PILE)
+
+
+def fourOfAKind():
+    pile_list = player_interaction.createPileList()
+    pile_list.reverse()
+    four_of_a_kind = False
+    counter = 4
+    print("pile_list[0] is", pile_list[0])      # debugging
+    top_card_int = value_cards.cardValueToInt(pile_list[0])
+    print("top_card_int is", top_card_int)      # debugging
+
+    for i in range(1, counter):
+        next_card_int = value_cards.cardValueToInt(pile_list[i])
+        print("next_card_int is", next_card_int)      # debugging
+        while top_card_int == next_card_int:
+            counter -= 1
+            print(counter)
+            break
+        if counter == 1:
+            four_of_a_kind = True
+    return four_of_a_kind
