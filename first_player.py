b6bd57f10c0cddf089ac8833bf208845baa487bc
magikitty <elina.satola@gmail.com>
2020-02-10 10:24:09 +0200

    Abstract, create lowestCardInHand()

--- first_player.py
+++ first_player.py
@@ -7,20 +7,24 @@ import value_cards
 def pickFirstPlayer():
     hand_player = data_cards.cardList(constants.HAND_PLAYER)
     hand_computer = data_cards.cardList(constants.HAND_COMPUTER)
+    print("lowest card player is", lowestCardInHand(hand_player))
+    print("lowest card computer is", lowestCardInHand(hand_computer))
 
+
+def lowestCardInHand(hand_list):
     card_lowest = "15"
 
-    for i in range(0, len(hand_player)):
-        card_hand_value = value_cards.cardValueToInt(hand_player[i])
+    for i in range(0, len(hand_list)):
+        card_hand_value = value_cards.cardValueToInt(hand_list[i])
         card_lowest_value = value_cards.cardValueToInt(card_lowest)
         print("1 the card value is", card_hand_value)     # debugging
 
         if card_hand_value < card_lowest_value:
-            card_lowest = hand_player[i]
+            card_lowest = hand_list[i]
             print("2 the lowest card is", card_lowest)     # debugging
 
         elif card_hand_value == card_lowest_value:
-            card_hand_suite = value_cards.getCardSuite(hand_player[i])
+            card_hand_suite = value_cards.getCardSuite(hand_list[i])
             print("3 card_hand_suite is", card_hand_suite)     # debugging
             card_lowest_suite = value_cards.getCardSuite(card_lowest)
             print("3 card_lowest_suite is", card_lowest_suite)     # debugging
@@ -29,8 +33,9 @@ def pickFirstPlayer():
             card_lowest_suite_value = constants.card_suite_values[card_lowest_suite]
 
             if card_hand_suite_value < card_lowest_suite_value:
-                card_lowest = hand_player[i]
+                card_lowest = hand_list[i]
             print("3 the lowest card is", card_lowest)     # debugging
+    return card_lowest
 
 
 
