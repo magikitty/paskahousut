5cce4413f0dced4eeb5abf76ae35cae0e8924f28
magikitty <elina.satola@gmail.com>
2020-02-13 14:18:09 +0200

    Create playCardComputer()

--- computer_action.py
+++ computer_action.py
@@ -1,9 +1,25 @@
 import constants
 import data_cards
 import data_pile_cards
+import data_read_write
 import random
 import rules
 import value_cards
+
+
+def playCardComputer():
+    if computerCardToPlay() != constants.ERROR_NO_CARD_TO_PLAY:
+        card_play = computerCardToPlay()
+        data_cards.removeCardFromHand(card_play, constants.HAND_COMPUTER)
+        print(constants.MESSAGE_COMPUTER_PLAYED_CARD, card_play)
+        if value_cards.cardValueToInt(card_play) == 10:
+            rules.foldPile()
+            # Write function to start computer's turn again
+        else:
+            data_read_write.addToFile("\n" + card_play, constants.PILE_CARDS)
+            if rules.fourOfAKind() == True:
+                rules.foldPile()
+                # Write function to start computer's turn again
 
 
 def computerCardToPlay():
@@ -35,4 +51,5 @@ def computerCardToPlay():
             print("Found card to play:", card_to_play)    # debugging
             return card_to_play
 
-    return print("ERROR no card to play found")    # debugging
+    print("ERROR no card to play found")    # debugging
+    return constants.ERROR_NO_CARD_TO_PLAY
