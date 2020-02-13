be4c016c433be0a6cbee7cb357a3fa3685eaa281
magikitty <elina.satola@gmail.com>
2020-02-13 19:22:07 +0200

    Add constants for computer player

--- constants.py
+++ constants.py
@@ -35,12 +35,11 @@ MESSAGE_PICK_PILE = "\nYou are picking up all the cards in the pile"
 MESSAGE_PLAY_CARD = "\nYou are playing a card from your hand"
 MESSAGE_SHOW_HAND = "\nHere are your cards:\n"
 
-# Status messages
+# Status messages for human player
 MESSAGE_ALL_CARDS_IN_PILE = "\nHere are all the cards in the pile. Card number 1 (at the top of the pile) is the most recent card played."
 MESSAGE_CANNOT_PLAY_CARD = "\nYou cannot play that card. Choose another card or do something else."
 MESSAGE_CHOOSE_ACTION = "\nWhat do you want to do? "
 MESSAGE_CHOOSE_CARD_TO_PLAY = "\nEnter the number of the card you want to play: "
-MESSAGE_COMPUTER_TURN = "\nIt's the computer's turn!"
 MESSAGE_EMPTY_DECK = "\nThere are no cards left in the deck. Do something else."
 MESSAGE_EMPTY_HAND = "\nYou don't have any cards left in your hand!"
 MESSAGE_EMPY_PILE = "\nThere are no cards in the pile. Do something else."
@@ -49,6 +48,14 @@ MESSAGE_PILE_EMPTY = "\nThere are no cards in the pile"
 MESSAGE_PLAYED_CARD = "\nYou play the card:"
 MESSAGE_PLAYER_TURN = "\nIt's the player's turn!"
 MESSAGE_TOP_CARD_IN_PILE = "\nHere is the card at the top of the pile:"
+
+# Status messages for computer player
+MESSAGE_COMPUTER_PLAYED_CARD = "\nThe computer has played the card:"
+MESSAGE_COMPUTER_TURN = "\nIt's the computer's turn!"
+MESSAGE_COMPUTER_PICK_PILE = "\nComputer has picked up all the cards in the pile."
+
+# Internal Message
+INTERNAL_NO_CARD_TO_PLAY = "\nERROR: No card to play found\n"
 
 # Dictionary with card suite values
 card_suite_values = {"Clubs": 1, "Diamonds": 2, "Spades": 3, "Hearts": 4}
