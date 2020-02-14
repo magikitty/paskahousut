23f7bb71b803d837968aa56a4702f79027dd453e
magikitty <elina.satola@gmail.com>
2020-02-14 13:47:52 +0200

    Create turn.py, move playerTurn() and computerTurn() to turn.py

---
+++ turn.py
@@ -0,0 +1,21 @@
+import computer_action
+import constants
+import data_cards
+import data_pile_cards
+import player_interaction
+
+
+# PlayerTurn starts player's turn
+def playerTurn():
+    print(constants.MESSAGE_PLAYER_TURN)
+    data_cards.dealCardToPlayer()
+    player_interaction.displayPlayerHandNumbered()
+    data_pile_cards.displayPileTopCard()
+    player_interaction.processPlayerTurn()
+
+
+# ComputerTurn starts computer's turn
+def ComputerTurn():
+    print(constants.MESSAGE_COMPUTER_TURN)
+    data_cards.dealCardToComputer()
+    computer_action.processComputerTurn()
