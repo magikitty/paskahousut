d95c1311e7077629cb5cbe448fcc5768a6f9c03c
magikitty <elina.satola@gmail.com>
2020-02-17 09:55:55 +0200

    Remove debugs

--- computer_action.py
+++ computer_action.py
@@ -42,38 +42,31 @@ def cardToPlayComputer():
     hand_computer = data_cards.cardList(constants.HAND_COMPUTER)
     hand_copy = []
     hand_copy.extend(hand_computer)
-    print(hand_copy)    # debugging
 
     # Get top pile card and value
     card_pile_top = data_pile_cards.getPileTopCard()
     card_pile_top_int = value_cards.cardValueToInt(card_pile_top)
-    print("card to beat is:", card_pile_top_int)    # debugging
     # If top pile card = 3
     if card_pile_top_int == 3:
         card_pile_top_int = value_cards.cardValueToInt(data_pile_cards.cardUnderThree())
-        print("card to beat is:", card_pile_top_int)    # debugging
 
     # Loop until card to play is found, else return error
     while len(hand_copy) != 0:
         num = random.randint(0, (len(hand_copy) - 1))
-        print("num is", num)    # debugging
         card_to_play = hand_copy[num]
         hand_copy.pop(num)
-        print("card is", card_to_play)    # debugging
         card_to_play_int = value_cards.cardValueToInt(card_to_play)
 
         if rules.checkCanPlayCard(card_to_play_int, card_pile_top_int) == True:
-            print("Found card to play:", card_to_play)    # debugging
             return card_to_play
 
-    print("ERROR no card to play found")    # debugging
     return constants.INTERNAL_NO_CARD_TO_PLAY
 
 
 def drawPlayCardComputer():
     card_drawn = data_cards.getRandomCard()
     data_cards.removeCardFromDeck(card_drawn)
-    print("Drew card:", card_drawn)     # debugging
+    print(constants.MESSAGE_COMPUTER_DREW_CARD, card_drawn)
     card_pile = data_pile_cards.getPileTopCard()
 
     card_drawn_int = value_cards.cardValueToInt(card_drawn)
